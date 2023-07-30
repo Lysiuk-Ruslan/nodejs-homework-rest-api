4077f62d0acf9863fc57d5a9ab07e03b821ea308
Lisiuk-Ruslan <114481433+Lysiuk-Ruslan@users.noreply.github.com>
2023-07-30 20:16:10 +0300

    Правки ментора

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -9,15 +9,16 @@ const { HttpError, ctrlWrapper } = require("../utils");
 const listContacts = async (req, res) => {
     const { _id: owner } = req.user;
 
-    const { page = 1, limit = 20, favorite } = req.query;
-    const skip = (page - 1) * limit;
+    // const { page = 1, limit = 20 } = req.query;
+    // const skip = (page - 1) * limit;
 
-    const filter = { owner };
-    if (favorite && favorite === "true") {
-        filter.favorite = true;
-    };
+    // const filter = { owner };
+    // if (favorite && favorite === "true") {
+    //     filter.favorite = true;
+    // };
+    // const result = await Contact.find({ owner }, { skip, limit });
 
-    const result = await Contact.find(filter, { skip, limit });
+    const result = await Contact.find({ owner });
     res.json(result)
 }
 
